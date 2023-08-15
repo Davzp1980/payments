f8b85f1eb956a6018b6c0a068c6fef3008667786
Davzp1980 <davzp1980@gmail.com>
2023-08-15 20:16:08 +0300

    Created models & postgres tables

--- postgres.go
+++ postgres.go
@@ -15,20 +15,6 @@ func CreateNewDB_Admins_Users(db *sql.DB) error {
 			name VARCHAR (25) NOT NULL UNIQUE,
 			password_hash VARCHAR NOT NULL UNIQUE
 		);
-		CREATE TABLE IF NOT EXISTS usercabinet (
-			id serial PRYMARY KEY,
-			user_name VARCHAR (25),
-			iban VARCHAR,
-			AccountBalance serial
-		);
-		CREATE TABLE IF NOT EXISTS payment (
-			id serial PRYMARY KEY,
-			user_name VARCHAR (25),
-			number_payment VARCHAR,
-			amount_payment serial,
-			date TIMESTAMP WITH TIME ZONE
-		);
-
 	`
 
 	_, err := db.Exec(CreateTablesQuery)
