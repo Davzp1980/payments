bea184b111339d6c5f1c070ac06dd09e97ecc22b
Dav1980 <davzp1980@gmail.com>
2023-09-02 13:48:16 +0300

    изменены файлы миграции

---
+++ schema/000001_init.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS users (
+			id serial PRIMARY KEY,
+			name VARCHAR (25) NOT NULL UNIQUE,
+			password VARCHAR NOT NULL UNIQUE,
+			is_admin boolean,
+			blocked boolean DEFAULT false
+		);
+		CREATE TABLE IF NOT EXISTS accounts (
+			id serial PRIMARY KEY,
+			user_id serial NOT NULL,
+			iban VARCHAR (34) NOT NULL UNIQUE,
+			balance serial,
+			blocked boolean
+
+		);
+		CREATE TABLE IF NOT EXISTS payments (
+			id serial PRIMARY KEY,
+			user_id serial NOT NULL,
+			reciever VARCHAR,
+			reciever_iban VARCHAR,
+			payer VARCHAR,
+			payer_iban VARCHAR,
+			amount_payment serial,
+			date TIMESTAMP WITH TIME ZONE
+		);
